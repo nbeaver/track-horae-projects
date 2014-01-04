863d189a0e71c806bf5fc6c0fb5b4c0ad2fb1562
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-01-04 12:22:24 -0600

    Changed single quotes to double quotes, to make an alias in bashrc easier.

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 git init
 git config diff.zip.textconv zcat
-echo '*.prj diff=zip' > .gitattributes
+echo "*.prj diff=zip" > .gitattributes
 # Reference: http://git-scm.com/book/en/Customizing-Git-Git-Attributes
